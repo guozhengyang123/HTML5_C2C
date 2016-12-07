a04e24a2d2d64fd5fe6df4061f3cb603b089e25f
zhaoboyan <996433896@qq.com>
2016-12-07 13:33:09 +0800

    编写轮播，分类，商品，会员信息控制器，编写轮播，分类，商品，会员信息的主页面，增加页面，修改页面，为各页面添加了main.css和table.css,index页面的部分修改

--- 后台/Application/Admin/Controller/CategoryController.class.php
+++ 后台/Application/Admin/Controller/CategoryController.class.php
@@ -2,13 +2,91 @@
 namespace Admin\Controller;
 use Think\Controller;
 class CategoryController extends Controller {
-    public function index(){
+    public function cate(){
         //1、获取数据
-    	$categoryModel = M('categorys');
-    	$categorys = $categoryModel -> select();
-    	//2、分配数据
-    	$this->assign('categorys',$categorys);
-    	//3、显示视图
-    	$this->display();
+    	$categoryModel = M('category');
+
+        $data = $categoryModel->select();
+        $data2=$categoryModel->select();
+        $this->assign('category',$data2);
+        //dump($data);
+        $this->assign('pid',$data);
+        $this->display();
+    
     }
+    public function create(){
+        // 1、分配分类数据
+
+        // 实例化User模型
+        $categoryModel = M('category');
+        // 取到所有对象
+        $data = $categoryModel -> select();
+        //分配数据
+        $data = $this->assign('category',$data);
+
+        //2、显示视图文件
+        $this->display();
+    }
+    public function cateadd(){
+        $categoryModel = M('category');
+        $result = $categoryModel->where("tag=1")->select();
+        $this->assign('result',$result);
+        $this->display();
+    }
+    public function store(){
+              
+            //$this->success('上传成功！');
+            //创建模型
+            $categoryModel = M('category');
+            //组织数据
+            $data = $categoryModel->create();
+
+            //设置thumb字段属性(目录+名字)
+            $data['name']=I('name');
+            $data['pid']=I('pid');
+            $data['tag']=I('tag');
+            //添加
+            if($categoryModel->add($data)){
+                $this->success('数据添加成功', 'cate');
+            }else{
+                $this->showError('数据添加失败');
+            }
+        }
+        public function destory(){
+        $id = I('id');
+         $categoryModel = M('category');
+        if($categoryModel->where("id=$id")->delete())
+        {
+            $this->success('数据删除成功');
+        }
+        else
+        {
+            $this->error('数据删除失败');
+        }
+    }
+
+    public function cateedit(){
+        $id = I('id');
+        $cateModel = M('category');
+        $data = $cateModel->find($id);
+        $result = $cateModel->where("tag=1")->select();
+        $this->assign('result',$result);
+        $this->assign('category',$data);
+        $this->display();
+    }
+    public function update(){
+        $id = I('id');
+        $cateModel = M('category');
+        $data = array();
+        $data['pid'] = I('pid');
+        $data['name'] = I('name');
+        $data['tag'] = I('tag');
+        $result = $cateModel->where("id=$id")->save($data);
+        if($result){
+                $this->success('数据添加成功', 'cate');
+            }else{
+                $this->showError('数据添加失败');
+            }
+    }
+
 }
--- 后台/Application/Admin/Controller/GoodsController.class.php
+++ 后台/Application/Admin/Controller/GoodsController.class.php
@@ -2,7 +2,56 @@
 namespace Admin\Controller;
 use Think\Controller;
 class GoodsController extends Controller {
-    public function index(){
-         $this->display();
+    public function goods(){
+        $goodsModel = M('goods');
+        $data = $goodsModel->select();
+        $this->assign('goods', $data);
+        $this->display();
     }
+    public function cate(){
+        $categoryModel = M('category');
+
+        $data = $categoryModel->select();
+        $data2=$categoryModel->select();
+        $this->assign('category',$data2);
+        //dump($data);
+        $this->assign('pid',$data);
+        $this->display();
+
+    }
+
+    public function cateadd(){
+        $categoryModel = M('category');
+        $result = $categoryModel->where("tag=1")->select();
+        $this->assign('result',$result);
+        $this->display();
+    }
+     public function delete(){
+            $id = I('id');
+            // 实例化User对象
+            $goodsModel = M('goods');
+            // 删除id为$id的用户数据
+            if($goodsModel->where("id=$id")->delete())
+            {
+                $this->success('删除成功');
+            }
+            else
+            {
+                $this->error('删除失败');
+            }
+    }
+
+    public function destory(){
+         $id = I('id');
+         $goodsModel = M('goods');
+        if($goodsModel->where("id=$id")->delete())
+        {
+            $this->success('数据删除成功');
+        }
+        else
+        {
+            $this->error('数据删除失败');
+        }
+    }
+
 }
