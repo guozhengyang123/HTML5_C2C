d71eaf62f5af2dd4dcd2beb28e8c1a7674b69fab
guozhengyang123 <251490228@qq.com>
2016-12-12 19:57:22 +0800

    change DengluzhuceController.class.php and  denglu.html and index.html

--- 后台/Application/Home/Controller/DengluzhuceController.class.php
+++ 后台/Application/Home/Controller/DengluzhuceController.class.php
@@ -4,13 +4,42 @@ use Think\Controller;
 class DengluzhuceController extends Controller {
     public function denglu(){
                
-        $this->display();
-        //获取酒店简介
-    }
+       if(IS_POST){
+                 $VipUsersModel=M('user');
+			     $condition=array(
+				'username'=>I('post.username'),
+				'password'=>I('post.password')
+				);
+			    //dump($condition);
+				$_SESSION['username']=I('post.username');
+				$_SESSION['password']=I('post.password');
+				$result=$VipUsersModel->where($condition)->count();
+				$username=I('post.username');
+				$password=I('post.password');
+				//dump($username);
+				//dump($password);
+				//dump($result);
+				if($result){
+					session('username',I('post.username'));
+					session('password',I('post.password'));
+					$this->success("登陆成功", '/index.php/home/index/index' );
+					//redirect('/index.php/home/index/index',5,'登陆成功');
+                    //header('/index.php/home/index/index');
+					}
+				else{
+					$this->error("用户名或密码不正确");
+					}
+         }
+		else{
+			$this->display();
+		  }
+
+
+     }
      public function zhuce(){
-               
+
         $this->display();
-        //获取酒店简介
+
     }
      
 }
