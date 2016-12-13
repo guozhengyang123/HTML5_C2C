b0677680da5c6d8ecd7ae608163454678f95a82b
guozhengyang123 <251490228@qq.com>
2016-12-13 11:10:34 +0800

    change show.css and ShowController.class.php  and  img

---
+++ 后台/Application/Home/Controller/ShowController.class.php
@@ -0,0 +1,12 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class ShowController extends Controller {
+    public function message(){
+         
+        $this->display();
+       
+    }
+
+   
+}
