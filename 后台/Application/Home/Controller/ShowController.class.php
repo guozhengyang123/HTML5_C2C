a8cd8b4662e692d8cb58b0f41879b5bbfaf39825
guozhengyang123 <251490228@qq.com>
2016-12-13 20:17:14 +0800

    change show.html and ShowController.class.php

--- 后台/Application/Home/Controller/ShowController.class.php
+++ 后台/Application/Home/Controller/ShowController.class.php
@@ -3,8 +3,15 @@ namespace Home\Controller;
 use Think\Controller;
 class ShowController extends Controller {
     public function message(){
-         
-        $this->display();
+         $text= I('post.text');
+         $messageModel = M('message');
+         $data = array();
+         $data['goodsId'] = I('get.goodsId');
+         $data['wr_info'] = I('post.text');
+         //$data['time'] = data("Y-m-d H:i:s",time());
+         $result = $messageModel->add($data);
+         $this->assign('result',$result);
+         $this->display('show');
        
     }
 
