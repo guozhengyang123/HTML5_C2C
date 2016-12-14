39da5a743d6220e097e362ba0f0f84a0d9daf6cb
byx1216 <byx1216@126.com>
2016-12-14 19:13:38 +0800

    publish页的后台搭建

--- 后台/Application/Home/Conf/config.php
+++ 后台/Application/Home/Conf/config.php
@@ -1,4 +1,14 @@
 <?php
 return array(
 	//'配置项'=>'配置值'
+	'FILE_UPLOAD' => array( 
+            'maxSize'    =>    3145728,
+            'rootpath'   =>    THINK_PATH,
+            'savePath'   =>    '../Public/uploads/',
+            // 'savePath'   =>    'Uploads/',
+            'saveName'   =>    date('YmdHis',time()).'_'.mt_rand(1000,9999),  //array('uniqid',date('Ymd',time())),
+            'exts'       =>    array('jpg', 'gif', 'png', 'jpeg'),
+            'autoSub'    =>    true, 
+            'subName'    =>    array('date','Y-m-d'),
+            )
 );
--- 后台/Application/Home/Controller/PublishController.class.php
+++ 后台/Application/Home/Controller/PublishController.class.php
@@ -1,20 +1,78 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+use Think\Upload;
 class PublishController extends Controller {
+
     public function publish(){
+    	$data=M('category');
+        $father=$data->where('tag=1')->select();
+        $this->assign('cate',$father);          		
+		$this->display();
+    }//publish()
+    public function get(){
+    	if (IS_POST) {
+    		$cateid = I('post.name');
+    		$data=M('category');
+    		$cate=$data->where("pid=$cateid")->select();
+    		$this->ajaxReturn($cate,'json');
+    	}else{
+    		//echo("出错了");
+    		$name[name]="lisi";
+    		$name[id]=1;
+    		$this->ajaxReturn($name,'json');
+    	// }
+    	}
+    	
 
-        $class=M("category");
-        $data=$class->select();
-  
-        $list=array();
-        foreach($data as $arr){
-            $list[$arr['pid']][]=$arr;//重新整理数据排序。
-        }
-      
-        $this->assign("list",$list);
-        $this->display();
+    }//get()
+	
+	public function uploadfile(){    
 
-    }
-   
+		
+		if (IS_POST) {
+			$user = M('pub');
+			//$data['name'] = $_POST['name'];
+			$data=$_POST;
+			session('[start]'); 
+			// session('id','5');  
+			$data['username']= session('id');
+			$data['time']=date("Y-m-d" ,time());
+
+			// 取得成功上传的文件信息
+			//$info=$upload()->upload();
+			// 保存当前数据对象
+			
+			$upload = new Upload(C('FILE_UPLOAD'));
+			$info = $upload -> upload($_FILES);
+			//dump($info);
+			if (!$info) {
+				# code...
+				dump($upload->getError());
+				exit;
+			}else{
+				foreach ($info as $file) {
+					# code...
+					$imageurl = $file['rootpath'].$file['savepath'].$file['savename'];
+
+				}
+			}
+			//dump($imageurl);
+			$data['imgurl1'] = $imageurl;
+			
+
+
+			$result=$user->data($data)->add();
+			//var_dump($result);
+			if($result){    //设置成功后跳转页面的地址，默认的返回页面是$_SERVER['HTTP_REFERER']    								
+				//var_dump($data);
+				 echo "<script>alert('商品发布成功！');location.href='".$_SERVER["HTTP_REFERER"]."';</script>";
+			} 
+			else {    //错误页面的默认跳转页面是返回前一页，通常不需要设置    
+				echo "<script>alert('上传失败，请重新提交');history.go(-1)</script>"; 
+			}
+		 }
+
+
+	}// uploadfile()
 }
